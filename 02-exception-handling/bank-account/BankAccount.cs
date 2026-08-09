b60c029fe943938392e94d1f01507c42c49517ac
AZILE GOMOMO <gomomoazile68@gmail.com>
2026-08-09 23:30:38 +0200

    Add exception handling exercises

---
+++ 02-exception-handling/bank-account/BankAccount.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        public BankAccount()
+        {
+            accountNumber = string.Empty;
+            balance = 0;
+        }
+
+        public string  AccountNumber
+        {
+            get { return accountNumber; }
+            set { accountNumber = value; }
+        }
+
+        public double GetBalance() 
+        {
+            return balance;
+        }
+
+        public  void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > balance)
+            {
+                throw new InsufficientFundsException("not enough funds!");
+                
+            }
+            else
+            {
+                balance -= amount;
+            }
+        }
+
+        public void Transfer(BankAccount target, double amount)
+        {
+            try
+            {
+                Withdraw(amount);
+
+                target.Deposit(amount);
+                Console.WriteLine("Transfer of "+ amount+ " to target account is successful");
+            }
+            catch (InsufficientFundsException ex)
+            {
+                Console.WriteLine("Transfer failed: "+ ex.Message);
+            }
+
+           
+
+           
+        }
+    }
+
+    class InsufficientFundsException : Exception
+    {
+        public InsufficientFundsException(string message) : base(message)
+        {
+
+        }
+    }
+    
+}
---
+++ 02-exception-handling/bank-account/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            BankAccount bankAccount = new BankAccount();
+            try
+            {
+
+                Console.Write("Enter amount to deposit: ");
+                double amount = Convert.ToDouble(Console.ReadLine());
+
+                account.Deposit(amount);
+                Console.WriteLine("Balance after deposit: "+ account.GetBalance());
+
+                Console.Write("\nEnter amount to withdraw: ");
+                int amountWithdrawn = Convert.ToInt32(Console.ReadLine());
+                account.Withdraw(amountWithdrawn);
+                Console.WriteLine("Balance after withdrawal: "+ account.GetBalance() );
+
+                Console.Write("enter another amount to withdraw, make sure it's more than balance: ");
+                int amountWithdrawnOver = Convert.ToInt32(Console.ReadLine());
+                account.Withdraw(amountWithdrawnOver);
+
+                account.Transfer(bankAccount, amountWithdrawn);
+
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine("input format error: " + exception.Message);
+            }
+            catch (InsufficientFundsException message)
+            {
+                Console.WriteLine("insufficient funds error: " + message.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Transaction Complete");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
